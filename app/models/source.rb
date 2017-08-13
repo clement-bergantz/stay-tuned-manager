c4f30da5f4c918f20c1afe53aa12c2cf2c20bf61
clement bergantz <bergantz.clement@gmail.com>
2017-08-13 20:50:40 +0200

    types table seeded and model validations done

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,3 +1,4 @@
 class Source < ApplicationRecord
   belongs_to :type
+  has_many :reviews
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,2 +1,4 @@
 class Type < ApplicationRecord
+	has_many :sources
+	has_many :reviews, through: :sources
 end
